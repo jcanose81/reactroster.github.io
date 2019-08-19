3a5a3ff368ed560363f53994645d25646b5eeaed
Justin Canose <50759336+jcanose81@users.noreply.github.com>
2019-08-19 11:13:43 -0400

    initial commit

---
+++ precache-manifest.839c7cf4ca6a578822519b89ca1b4929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "110fbcee4e9455b79e48347d4ef5b63b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbfcf17ac5968bc545fd",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6c0faa7ae3a23ee61263",
+    "url": "/static/css/main.0d339e02.chunk.css"
+  },
+  {
+    "revision": "fbfcf17ac5968bc545fd",
+    "url": "/static/js/2.0f90415c.chunk.js"
+  },
+  {
+    "revision": "6c0faa7ae3a23ee61263",
+    "url": "/static/js/main.909c3a9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
